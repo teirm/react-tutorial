5ad85c0a46adfda7aa4a729626d6f3b33f127377
Cyrus Ramavarapu <tribunus.vii@gmail.com>
2021-12-25 14:18:19 -0500

    added draw message and bold on hover in history

--- src/index.js
+++ src/index.js
@@ -104,6 +104,8 @@ class Game extends React.Component {
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
+        } else if (this.state.stepNumber == 9) {
+            status = 'Draw game';
         } else {
             status = 'Next player: ' + 
                 (this.state.xIsNext ? 'X' : 'O');
